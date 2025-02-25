43c6607f29b47074a3ea853ffac1c66e98f97085
Nisa Rianti <165540983+Nisarianti2218@users.noreply.github.com>
2025-02-25 21:27:32 +0700

    add function Prima,modulus dan absolut

--- solution.js
+++ solution.js
@@ -49,57 +49,27 @@ function faktorial(n) {
 
 /**
  * Mengembalikan true jika angka adalah bilangan prima, false jika tidak.
- * @param {number} n 
- * @returns {boolean}
  */
 function isPrima(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false;
+    }
+    return true;
 }
 
 /**
  * Mengembalikan hasil dari a modulus b.
- * modulus adalah sisa pembagian a dengan b.
- * contoh 10 mod 3 = 1, karena 10 dibagi 3 adalah 3 sisa 1.
- * 10 / 3 = 3 sisa 1. dan 1 adalah jawabannya
- * @param {number} a 
- * @param {number} b 
- * @returns {number}
  */
 function modulus(a, b) {
+    return a % b;
 }
 
 /**
  * Mengembalikan nilai absolut dari sebuah angka.
- * @param {number} a 
- * @returns {number}
  */
 function absolut(a) {
-}
-
-
-/**
- * Mengembalikan nilai terbesar dari dua angka.
- * @param {number} a 
- * @param {number} b 
- * @returns {number}
- */
-function maksimum(a, b) {
-}
-
-/**
- * Mengembalikan nilai terkecil dari dua angka.
- * @param {number} a 
- * @param {number} b 
- * @returns {number}
- */
-function minimum(a, b) {
-}
-
-/**
- * Mengembalikan hasil dari a dibulatkan ke bilangan bulat terdekat.
- * @param {number} a 
- * @returns {number}
- */
-function bulatkan(a) {
+    return Math.abs(a);
 }
 
 // Export hasil akhir
@@ -110,4 +80,7 @@ module.exports = {
     bagi,
     pangkat,
     faktorial,
+    isPrima,
+    modulus,
+    absolut,
 };
